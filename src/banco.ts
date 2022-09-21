9a5acbe30f0e624c816ef139a8ee9091bd658d57
KaiqueBourguignon <112709239+KaiqueBourguignon@users.noreply.github.com>
2022-09-21 16:04:34 -0300

    Add files via upload

---
+++ src/banco.ts
@@ -0,0 +1,25 @@
+import PromptSync = require('prompt-sync');
+const prompt = PromptSync();
+
+function banco (){
+
+var login  = String (prompt ('PORFAVOR DIGITE SEU EMAIL:  '));
+var  senha = Number  (prompt ('PORFAVOR DIGITE SUA SENHA:  '));
+
+if (login == cliente.email && senha == cliente.senha ){
+    
+    console.log  ('    |$$$$$$$$$$$$$$$$$$$$$$$$$$$|    ');  
+    console.log  ('    |~~~ BEM VINDO A GENBANK ~~~|    ');
+    console.log  ('    |$$$$$$$$$$$$$$$$$$$$$$$$$$$|    '); 
+    
+}else {
+     
+    console.log('|XXXXXXXXXXXXXXXXXXXXXXXX|');
+    console.log('|XXX LONGIN REPROVADO XXX|');
+    console.log('|XXXXXXXXXXXXXXXXXXXXXXXX|');
+}
+
+}
+export { banco };
+
+
